04fab7f3ca29185c706d3c1b8de51ba3c573fdeb
Anastasia Saiz <71260261+AnastasiaSaiz@users.noreply.github.com>
2020-12-10 19:36:14 +0100

    modif

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import libreria from "./libreria";
+import Cabecera from "./cabecera";
+import Main from "./main";
+import Footer from "./footer";
+
+function App() {
+  const libros = libreria.libros.map(libro => {
+    return <Main key={libro.id} titulo={libro.titulo} autor={libro.autor} editoria={libro.editorial} descripcion={libro.descripcion} img={libro.img} stock={libro.stock} />
+  });
+  return (
+    <div>
+      <Cabecera nombre={libreria.usuario.nombre} vip={libreria.usuario.vip} imagen={libreria.usuario.imagen} />
+      {libros}
+      <Footer libros={libreria.libros} />
+    </div>
+
+  );
+
+
+}
+
+export default App;
